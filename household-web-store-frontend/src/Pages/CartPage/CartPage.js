46f93109e17faaf35c1d06088a9c99678949e092
Jakub Ozierski <jakub.ozierski@gmail.com>
2023-06-18 00:17:49 +0200

    add styles to cart

--- household-web-store-frontend/src/Pages/CartPage/CartPage.js
+++ household-web-store-frontend/src/Pages/CartPage/CartPage.js
@@ -1,21 +1,22 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
+import "./CartPage.css";
 
 const CartPage = () => {
-  const products = sessionStorage.getItem('product')
+  const products = sessionStorage.getItem('product');
   const array = JSON.parse("[" + products + "]");
-  console.log(array)
+  console.log(array);
   return (
     <div>
       <Header />
-      <div>CartPage</div>
-      {array[0].map((product)=>(
-        <div>
-          <img src={product.attributes.image}/>
-          <p>{product.attributes.product_name}</p>
-          <p>{product.attributes.price}</p>
-        </div>  
+      <div className="cart-page">Your Cart</div>
+      {array[0].map((product) => (
+        <div className="product">
+          <img className="product-image" src={product.attributes.image} alt={product.attributes.product_name} />
+          <p className="product-name">{product.attributes.product_name}</p>
+          <p className="product-price">{product.attributes.price} zł</p>
+        </div>
       ))}
       <Footer />
     </div>
