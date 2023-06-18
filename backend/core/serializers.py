ac0d3687230dcbbfbe455a39f7d882cb9979ea28
Arczi4 <urzedowski.artur@gmail.com>
2023-06-18 13:40:33 +0200

    Added new edpoint for creating users

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,19 +1,31 @@
 from . import models
 from rest_framework import serializers
 from rest_framework.fields import CharField, EmailField
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
+User = get_user_model()
 
 
 class ContactSerializer(serializers.ModelSerializer):
+    name = CharField(source="title", required=True)
+    message = CharField(source="description", required=True)
+    email = EmailField(required=True)
 
-	name = CharField(source="title", required=True)
-	message = CharField(source="description", required=True)
-	email = EmailField(required=True)
-	
-	class Meta:
-		model = models.Contact
-		fields = (
-			'name',
-			'email',
-			'message'
-		)
+    class Meta:
+        model = models.Contact
+        fields = ("name", "email", "message")
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "password")  # Include any additional fields here
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        print(validated_data)
+        user = User.objects.create_user(**validated_data)
+        token = Token.objects.create(user=user)
+        
+        return user, token
