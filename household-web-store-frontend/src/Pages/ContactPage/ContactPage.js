25f6b60c1eeaab66f1a6f532b105fc216c3b5e3f
Jakub Ozierski <jakub.ozierski@gmail.com>
2023-06-18 17:19:57 +0200

    add styles to login, register. add contact page

--- household-web-store-frontend/src/Pages/ContactPage/ContactPage.js
+++ household-web-store-frontend/src/Pages/ContactPage/ContactPage.js
@@ -1,14 +1,39 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
+import MapContainer from "../../Components/MapContainer/MapContainer";
+import "./ContactPage.css";
 
 const ContactPage = () => {
-  // product_name = sessionStorage.getItem('product_name')
   return (
-    <div>
+    <div className="contact-container">
       <Header />
-      <div>ContactPage</div>
-      {/* <div>{product_name}</div> */}
+      <div className="map-container">
+        <MapContainer />
+      </div>
+      <div className="contact-content">
+        <div className="row">
+          <div className="column">
+            <h3>Contact</h3>
+            <p>It's very easy to get in touch with us!</p>
+          </div>
+          <div className="column">
+            <h3>Phone</h3>
+            <p>111 222 333</p>
+          </div>
+          <div className="column">
+            <h3>E-mail</h3>
+            <p>shopping@dka.pl</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column address-column">
+            <h3>Address</h3>
+            <p>Zakładowa 138</p>
+            <p>58-500 Wrocław</p>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
